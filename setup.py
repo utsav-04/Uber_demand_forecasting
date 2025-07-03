f4587161054e50172242b0a9eeeb465e941253b1
utsav-04 <utsavraj.ptn04@gmail.com>
2025-07-04 00:21:12 +0530

    Initialized a new Repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is helping the Uber driver to get the forecast of the reion with high demand of passenger.',
+    author='Utsav Raj',
+    license='',
+)
